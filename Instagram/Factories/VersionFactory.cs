224f442293fda73121c70253ccd5bbcb0ab42433
GekkeVent <GekkeVent@users.noreply.github.com>
2016-10-12 04:46:28 +0200

    Update VersionFactory.cs

--- Instagram/Factories/VersionFactory.cs
+++ Instagram/Factories/VersionFactory.cs
@@ -22,7 +22,9 @@ namespace Ninja.Instagram.API.Factories
             _versions = new Dictionary<string, string>()
             {
                 {"9b3b9e55988c954e51477da115c58ae82dcae7ac01c735b4443a3c5923cb593a", "8.0.0"},
-                {"3d3d669cedc38f2ea7d198840e0648db3738224a0f661aa6a2c1e77dfa964a1e", "8.4.0"}
+                {"3d3d669cedc38f2ea7d198840e0648db3738224a0f661aa6a2c1e77dfa964a1e", "8.4.0"},
+                {"b5d839444818714bdab3e288e6da9b515f85b000b6e6b452552bfd399cb56cf0", "8.5.1"},
+                {"012a54f51c49aa8c5c322416ab1410909add32c966bbaa0fe3dc58ac43fd7ede", "9.2.0"}
             };
             _rnd = new Random(Environment.TickCount);
         }
